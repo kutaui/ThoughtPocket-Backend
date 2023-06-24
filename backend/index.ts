81084127dcf752414ef184d92753b864dd3a6928
Kutay <kutaybekleric@gmail.com>
2023-06-25 00:53:27 +0300

    fix:samesite none for production

--- backend/index.ts
+++ backend/index.ts
@@ -21,9 +21,9 @@ const corsOptions = {
 
 const app = express();
 app.use(cors(corsOptions))
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoutes);
--- backend/utils/generateToken.ts
+++ backend/utils/generateToken.ts
@@ -10,7 +10,7 @@ const generateToken = (res: Response, userId: Types.ObjectId) => {
     res.cookie("jwt",token,{
         httpOnly: true,
         secure: process.env.NODE_ENV !== "development",
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
     })
 }
