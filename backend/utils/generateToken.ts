635d815bed55f05cb573db3293660921a808982c
Kutay <kutaybekleric@gmail.com>
2023-06-25 16:57:31 +0300

    fix samesite

--- backend/utils/generateToken.ts
+++ backend/utils/generateToken.ts
@@ -11,7 +11,6 @@ const generateToken = (res: Response, userId: Types.ObjectId) => {
         httpOnly: true,
         secure: process.env.NODE_ENV !== "development",
         sameSite: "none",
-
         maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
 
     })
