764cde6091c1f8b3e378136092fcd182751bc9b6
Kutay <kutaybekleric@gmail.com>
2023-07-14 17:20:17 +0300

    fix token sign

--- backend/utils/generateToken.ts
+++ backend/utils/generateToken.ts
@@ -5,15 +5,15 @@ import {Types} from "mongoose";
 // We add the userId to the payload of the token to validate the user later
 const generateToken = (res: Response, userId: Types.ObjectId) => {
 // create a token
-    const token = jwt.sign({userId}, process.env.JWT_SECRET as string, {
-        expiresIn: "30d"
-    })
+    const token = jwt.sign({userId, iss: 'trying', aud: 'audience'}, process.env.JWT_SECRET as string, {
+        expiresIn: '30d'
+    });
 // send a token as a cookie
     res.cookie("jwt", token, {
-        httpOnly: false,
+        httpOnly: true,
         secure: false,
         // this should be strict to prevent CSRF attacks but I can't do that because the frontend is on a different domain
-        sameSite: "none",
+        sameSite: "strict",
         maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
 
     })
